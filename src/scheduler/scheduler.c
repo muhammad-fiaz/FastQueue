badd0df17c945c397e6f0fdae1d86940010abe18
Muhammad Fiaz <contact@muhammadfiaz.com>
2026-07-15 16:02:08 +0530

    fix: race condition in scheduler - increment tasks_completed before decrementing tasks_active

--- src/scheduler/scheduler.c
+++ src/scheduler/scheduler.c
@@ -109,9 +109,9 @@ static int worker_main(void *arg)
 
             fq_task_destroy(task);
 
-            fq_atomic_fetch_sub_explicit(&s->tasks_active, 1,
-                                         FQ_MEMORY_ORDER_RELEASE);
             fq_atomic_fetch_add_explicit(&s->tasks_completed, 1,
+                                         FQ_MEMORY_ORDER_RELEASE);
+            fq_atomic_fetch_sub_explicit(&s->tasks_active, 1,
                                          FQ_MEMORY_ORDER_RELEASE);
         } else {
             fq_atomic_fetch_add_explicit(&s->idle_count, 1,
